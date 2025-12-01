311f222609111e7eda55e25e4e7bb27ace5e6269
m6998499 <m6998499@gmail.com>
2025-12-01 05:42:55 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,8 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/loadboard/:path*",
-    "/post-load/:path*",
+    "/",             // lock homepage
+    "/loadboard/:path*", 
+    "/post-load/:path*"
   ],
 };
