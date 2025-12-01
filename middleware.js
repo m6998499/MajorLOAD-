a6f1665efa910aa5d4d019a13675d05eafda213c
m6998499 <m6998499@gmail.com>
2025-12-01 05:48:55 -0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,8 +2,8 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/",             // lock homepage
-    "/loadboard/:path*", 
-    "/post-load/:path*"
+    "/",              // homepage
+    "/loadboard/:path*",   // lock /loadboard and anything inside it
+    "/post-load/:path*",   // lock /post-load and anything inside it
   ],
 };
