51d085134ca0881f10100ccd5dde78379302a0ca
m6998499 <m6998499@gmail.com>
2025-12-01 03:10:48 -0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-import "./globals.css";
-import NavBar from "./components/NavBar";
+import "../globals.css";
+import NavBar from "../components/NavBar";
 
 export const metadata = {
   title: "MajorLoad",
