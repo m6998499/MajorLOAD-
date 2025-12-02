66fdbcc6ee1a97a36be7597fea62d5438b5cf529
m6998499 <m6998499@gmail.com>
2025-12-01 22:53:59 -0500

    Update authSettings.js

--- src/lib/authSettings.js
+++ src/lib/authSettings.js
@@ -1,26 +1,3 @@
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
-// FIXED: Use ./db because they are neighbors in the same folder
-import { db } from "./db"; 
-
-export const authOptions = {
-  adapter: PrismaAdapter(db),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub;
-      }
-      return session;
-    },
-  },
-};
+// src/lib/authSettings.js
+import { db } from "./db"; // <--- This looks for db.js
+// ... rest of code
