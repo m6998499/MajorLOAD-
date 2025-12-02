7180f6b8a72fd6b4c3b6ff68993794fabd229638
m6998499 <m6998499@gmail.com>
2025-12-01 22:30:27 -0500

    Create authSettings.js

---
+++ src/lib/authSettings.js
@@ -0,0 +1,24 @@
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db"; 
+
+export const authOptions = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+};
