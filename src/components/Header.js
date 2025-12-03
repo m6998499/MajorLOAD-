6e3e290f525b5a23896afd5fe9b5854aa018d5d8
m6998499 <m6998499@gmail.com>
2025-12-02 23:09:37 -0500

    Trigger deployment for disclaimer link visibility
    Exclude Header component from rendering on login and home pages.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+// Header component with navigation links including disclaimer
 
 export default function Header() { 
   const pathname = usePathname();
